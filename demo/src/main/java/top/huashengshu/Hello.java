0c1efa856d505b3950774b5e0a437c6b98800f53
git第一次使用安装 <lzb1254863481@qq.com>
2020-05-07 17:34:14 +0800

    测试提交

--- demo/src/main/java/top/huashengshu/Hello.java
+++ demo/src/main/java/top/huashengshu/Hello.java
@@ -2,9 +2,6 @@ package top.huashengshu;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello LvQiFeng");
-        System.out.printf("项目发起者的提交测试！");
-        System.out.println("我是angel");
-        System.out.printf("ssssssssssss");
+        System.out.println("hello LvQiFeng");//
     }
 }
