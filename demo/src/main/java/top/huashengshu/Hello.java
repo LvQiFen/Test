db9995011b035bb9d5aa3b89b6e70a95eb583fb1
git第一次使用安装 <lzb1254863481@qq.com>
2020-05-07 17:36:13 +0800

    测试提交ss

--- demo/src/main/java/top/huashengshu/Hello.java
+++ demo/src/main/java/top/huashengshu/Hello.java
@@ -3,5 +3,6 @@ package top.huashengshu;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello LvQiFeng");//1
+        System.out.printf("hahahah");
     }
 }
