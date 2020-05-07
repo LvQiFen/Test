0995ade540f2a43d9d1ddff06969726a9407a2fd
git第一次使用安装 <lzb1254863481@qq.com>
2020-05-07 17:35:07 +0800

    测试提交

--- demo/src/main/java/top/huashengshu/Hello.java
+++ demo/src/main/java/top/huashengshu/Hello.java
@@ -2,6 +2,6 @@ package top.huashengshu;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello LvQiFeng");//
+        System.out.println("hello LvQiFeng");//1
     }
 }
