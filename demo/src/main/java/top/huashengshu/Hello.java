f395cfad89e9d1da18111d8d49455d9e4ce9ca8a
QiFeng <tenqifeng@163.com>
2020-05-11 16:04:58 +0800

    测试提交怎么回事

--- demo/src/main/java/top/huashengshu/Hello.java
+++ demo/src/main/java/top/huashengshu/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello LvQiFeng");//1
         System.out.printf("hahahah");
+        System.out.println("怎么回事？");
     }
 }
